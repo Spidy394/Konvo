0a4e4246f914073464eafd20535c206af58ae569
Spidy394 <mondalshubhodeep30924@gmail.com>
2025-06-02 17:44:37 +0530

    initial setup && add Signup

---
+++ server/src/lib/utils.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRECT, {
+        expiresIn: "7d",
+    })
+
+    res.cookie("jwt", token, {
+        maxAag: 7 * 24 * 60 * 60 * 1000, // 7d in milisecond 
+        httpOnly: true, // prevent XSS attacks
+        sameSite: "strict", // prevent CSRF attacks
+        secure: process.env.NODE_ENV !== "development",
+    })
+
+    return token;
+}
---
+++ server/src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+
+    fullName: {
+        type: String,
+        require: true,
+    },
+
+    userName: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+
+    bio: {
+        type: String,
+    },
+
+    password: {
+        type: String,
+        require: true,
+        minlenght: 6,
+    },
+
+    profilePic: {
+        type: String,
+        default: "",
+    },
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
