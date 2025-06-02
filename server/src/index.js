0a64c1cd76e9b5273b8e10a177d5d25bebb98018
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2025-06-03 00:36:39 +0530

    First phase server Done

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookiePerser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js"
 
 dotenv.config();
@@ -10,7 +11,9 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookiePerser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
