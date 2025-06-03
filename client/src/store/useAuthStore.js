dbcb62c2a2656241c8d25ca243196f5cf0dafa45
Spidy394 <mondalshubhodeep30924@gmail.com>
2025-06-03 17:48:22 +0530

    signup page

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -18,5 +18,10 @@ export const useAuthStore = create((set) => ({
         } finally{
             set({ isCheckingAuth: false });
         }
-    }
+    },
+
+    signup: async(data) => {
+        
+    },
 }));
+
