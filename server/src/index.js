527bf8465adfcc7639a0571b703783a42b223c0e
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2025-06-09 18:06:24 +0530

    deploy config

--- server/src/index.js
+++ server/src/index.js
@@ -24,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../../client/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
