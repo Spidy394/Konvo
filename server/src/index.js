aca21cf6d8b2707dfb235d4a4c501b39ef8f5c19
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2025-06-09 16:12:02 +0530

    deploy config

--- server/src/index.js
+++ server/src/index.js
@@ -6,9 +6,11 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js"
 import { app, server } from "./lib/socket.js";
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
@@ -20,6 +22,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
