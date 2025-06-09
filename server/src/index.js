fbe72a30f2106322df273282277d6c45743d8dea
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2025-06-09 17:15:58 +0530

    deploy config

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ import { app, server } from "./lib/socket.js";
 import path from 'path';
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
@@ -23,12 +23,12 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
+if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-  })
+  });
 }
 
 server.listen(PORT, () => {
